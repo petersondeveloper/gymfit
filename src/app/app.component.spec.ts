cb47cb1d4e67b62a171aae365012fb4b1b5d6973
Peterson <peterson.qna@gmail.com>
2024-07-13 00:14:59 -0300

    feat: name of the project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'smartfitdesafio' title`, () => {
+  it(`should have the 'gymfit' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('smartfitdesafio');
+    expect(app.title).toEqual('gymfit');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, smartfitdesafio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gymfit');
   });
 });
